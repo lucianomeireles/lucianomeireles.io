995dd68d5433078cb96e58fc4afe3556439ca883
Luciano Meireles <contato@lucianomeireles.io>
2017-07-15 17:03:56 -0300

    Header

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from './components/Home.vue'
-import About from './components/About.vue'
+import App from './views/App.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/about' },
+  { path: '/home', component: Home },
   { path: '/about', component: About }
 ]
 const router = new VueRouter({ routes, mode: 'history' })
-
-new Vue({ router }).$mount('#app')
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
